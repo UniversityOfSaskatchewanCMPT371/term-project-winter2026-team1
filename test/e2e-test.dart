c954e309f7afbe1fcdb58fb7199b0df8e234f65b
Matt Berry <matthewjberry03@gmail.com>
2026-02-13 11:34:48 -0600

    Created basic outline for e2e

--- test/e2e-test.dart
+++ test/e2e-test.dart
@@ -1,3 +1,24 @@
 /*
-
+Driver file for the full system end-to-end test. 
+This test will run the entire system, including initializing the front and backend,
+running through a basic set of runtime schenarios, testing basic features, then shutting down.
+E2E should output a final log of the test results, and any errors encountered during the test.
  */
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // 1. Start system. May need shell script to start containers? Need Theo
+
+  // 2. Run through basic runtime scenarios. These should be external files from test/integration, including:
+
+  // - Log in
+
+  // - Create new entry
+
+  // - Query
+
+  // - Export?
+
+  // 3. Shut down system. May need shell script to stop containers? Need Theo
+}
