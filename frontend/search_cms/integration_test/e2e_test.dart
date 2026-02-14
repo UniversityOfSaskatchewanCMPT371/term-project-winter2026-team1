dc5ada118c6cb1e8f76089903043714a1823b0c8
Matt Berry <matthewjberry03@gmail.com>
2026-02-14 10:45:59 -0600

    Added just some print statements to see if test is running Prints will be replaced with logging statements once logging is implemented Cant actually run this cuz I realized that the workflow it not on this branch And I can't run it locally cuz I dont want to download the full xcode package cuz its 20gb oopsies

--- frontend/search_cms/integration_test/e2e_test.dart
+++ frontend/search_cms/integration_test/e2e_test.dart
@@ -9,16 +9,23 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   // 1. Start system. May need shell script to start containers? Need Theo
+  // TODO: replace prints with log statements
+  print("Starting system...");
 
   // 2. Run through basic runtime scenarios. These should be external files from test/integration, including:
-
   // - Log in
+  print("Testing login...");
 
   // - Create new entry
+  print("Testing create new entry...");
 
   // - Query
+  print("Testing query...");
 
   // - Export?
+  print("Testing export...");
 
   // 3. Shut down system. May need shell script to stop containers? Need Theo
+  print("Shutting down system...");
+  print("E2E test completed.");
 }
