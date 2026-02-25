005c12602b01f6c2d8a1116b16043cec8206000f
Tyler Chow <tylerchow0@gmail.com>
2026-02-25 16:54:04 -0600

    Added more verbose comments for teaching mockito
    Issue #105
    authentication_sign_in_repository_impl_test.dart
    - moved the test cases into separate test calls so if one test case fails, the others will still run
    - added a test case that checks that a user with an empty password cannot login
    - wanted comments that explain what each command is doing for teaching mockito
    - added comments about when to use mocking
    - what the when() command does and how to use it

--- frontend/search_cms/test/features/authentication/data/repositories/authentication_sign_in_repository_impl_test.dart
+++ frontend/search_cms/test/features/authentication/data/repositories/authentication_sign_in_repository_impl_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:logging/logging.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:search_cms/core/utils/class_templates/result.dart';
@@ -7,7 +8,34 @@ import 'package:search_cms/features/authentication/data/models/user_model.dart';
 import 'package:search_cms/features/authentication/data/repositories/authentication_sign_in_repository_impl.dart';
 import 'package:search_cms/features/authentication/domain/entities/authentication_sign_in_result_classes.dart' as authentication_sign_in_result_classes;
 import 'package:search_cms/features/authentication/domain/entities/user_entity.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'authentication_sign_in_repository_impl_test.mocks.dart';
+
+
+/**TODO:
+ * figure out where logger outputs / where it should output
+ * add the tests to the test plan
+ * maybe add more tests
+ * add activity to the log
+ */
+
+
+
+/*
+To run write "flutter test" in the terminal (will run files with _test.dart in their name)
+
+when to use mocking
+when you want to unit test a class, say ClassToTest, which uses another class,
+you insert a mock of the other class for it to use. That also implies that ClassToTest has a method
+or parameter for a method that takes in an instance of the other class (which we will use to insert the mock instead)
+ex ClassToTest.someFunction(mock);
+ex AuthenticationSignInRepositoryImpl(api) //in the actual use it would be passed an instance of AuthenticationSignInApiImpl which is what we will be mocking
+
+The mocking of repository and api based on their interfaces let us ignore the details of the implementations and 
+explore all the possible routes in the class being tested. 
+This makes the tests reusable and relay the heavy-duty testing of actual API calls to integration testing.
+
+*/
 
 /*
   To use mocking, you need to run
@@ -20,32 +48,57 @@ import 'authentication_sign_in_repository_impl_test.mocks.dart';
  */
 @GenerateNiceMocks([MockSpec<AbstractAuthenticationSignInApi>()])
 void main() {
-  test('Test for the sign in function in AuthenticationSignInRepositoryImpl',
+  //  Logger.root.level = Level.ALL;
+  // final Logger logger = Logger('login tests');
+  // Logger.root.onRecord.listen((record) {
+  //   print('${record.level.name}: ${record.message}');
+  // });
+  // logger.shout('test on how to use logger'); 
+
+  //creating an instance of the class being mocked
+  final MockAbstractAuthenticationSignInApi mockApi = MockAbstractAuthenticationSignInApi();
+
+   // Define the response
+  /*
+  when(mock.function(is called with this input)).thenAnswer(this is what it will respond with)
+
+  basically when a mock's function is called with specified input, it will return what's in thenAnswer to pretend the mock did the real thing
+  */
+  when(mockApi.signIn('abc@abc.com', '123456')).thenAnswer(
+    (_) async => UserModel(
+      id: 'e0bc4427-2286-4773-ba74-c4491ba5f1be',
+      role: 'viewer',
+    ));
+
+  AuthenticationSignInRepositoryImpl authenticationSignInRepositoryImpl =
+    AuthenticationSignInRepositoryImpl(api: mockApi); //passing the class we're testing the mock
+
+  
+  test('Test whether a valid user can sign in with AuthenticationSignInRepositoryImpl.signIn()',
     () async {
 
-      final MockAbstractAuthenticationSignInApi mockApi = MockAbstractAuthenticationSignInApi();
-
-      // Define the response
-      when(mockApi.signIn('abc@abc.com', '123456')).thenAnswer(
-        (_) async => UserModel(
-          id: 'e0bc4427-2286-4773-ba74-c4491ba5f1be',
-          role: 'viewer',
-        ));
-
-      AuthenticationSignInRepositoryImpl authenticationSignInRepositoryImpl =
-        AuthenticationSignInRepositoryImpl(api: mockApi);
-
       // Test Case 1: This should log in successfully
-      final Result authResult1 = await authenticationSignInRepositoryImpl.signIn('abc@abc.com', '123456');
+      /*
+      one of the lines in authenticationSignInRepositoryImpl.signIn is await _api.signIn(email, password);
+      since we passed in the mock for api in the constructor for authenticationSignInRepositoryImpl, it will call mockApi.signIn instead
+      so you will want the inputs to match a when() created earlier (you can have multiple when()s)
+      */
+      final Result authResult1 = await authenticationSignInRepositoryImpl.signIn('abc@abc.com', '123456'); 
 
-      expect(authResult1.runtimeType,
+      expect(authResult1.runtimeType, //(actual output, expected output) thnk of it as actual output == expected output {keep going} else{fail and leave test()} where 
           authentication_sign_in_result_classes.Success);
       if (authResult1 is authentication_sign_in_result_classes.Success) {
         expect(authResult1.userEntity.id, 'e0bc4427-2286-4773-ba74-c4491ba5f1be');
         expect(authResult1.userEntity.role, Role.viewer);
-      } else {
-        fail('Expect authResult to be authentication_sign_in_result_classes.Success, but not true');
+
+      } else { //this is another way to say the test() failed, expect will also do it, but this works if == is not how you compare (like when )
+        fail('Expect authResult to be authentication_sign_in_result_classes.Success, but not true'); 
       }
+    }
+  );
+
+  test('Test for a valid user with a wrong password can sign in with AuthenticationSignInRepositoryImpl.signIn()',
+    () async {
 
       // Test Case 2: This should fail due to password miss match
       final Result authResult2 =
@@ -55,6 +108,12 @@ void main() {
       } else {
         fail('Expect authResult to be authentication_sign_in_result_classes.Failure, but not true');
       }
+
+    }
+  );
+
+  test('Test whether a non-existant user can sign in with AuthenticationSignInRepositoryImpl.signIn()',
+    () async {
 
       // Test Case 3: This should fail due to non-exist credential
       final Result authResult3 =
@@ -66,4 +125,18 @@ void main() {
       }
     }
   );
+
+
+  test('Test whether a user with an empty password can sign in with AuthenticationSignInRepositoryImpl.signIn()',
+    () async {
+
+      // Test Case 4: This should fail due to failing an assertion about acceptable password lengths
+      final Result authResult3 =
+        await authenticationSignInRepositoryImpl.signIn('admin@abc.com', '');
+      if (authResult3 is! authentication_sign_in_result_classes.Failure) {
+        fail('Expect authResult to be authentication_sign_in_result_classes.Failure, but not true');
+      }
+    }
+  );
+
 }
