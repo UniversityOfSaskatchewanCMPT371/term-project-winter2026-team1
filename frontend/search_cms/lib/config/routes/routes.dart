48c94693d4323914cf8d5ebda49fbe71412aa2c6
Jonathan Mutakura <jmutakura@gmail.com>
2026-03-02 21:32:38 -0600

    donne the following: - added the dashboard layout file using Drawer - removed homepage logic from the auth featuer folders - added relavent folders for dashboard features

--- frontend/search_cms/lib/config/routes/routes.dart
+++ frontend/search_cms/lib/config/routes/routes.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:search_cms/features/authentication/presentation/pages/home_page.dart';
 import 'package:search_cms/features/authentication/presentation/pages/login_page.dart';
+import 'package:search_cms/features/dashboard/presentation/pages/dashboard_layout.dart';
+import 'package:search_cms/features/dashboard/presentation/pages/dashboard_home_page.dart';
+import 'package:search_cms/features/dashboard/presentation/pages/dashboard_add_page.dart';
 
-/*
-  The route settings for the application.
-  Remember to define your routes here when you add new pages.
-  This is navigator 2.0.
-  Calls to navigator 1.0 are tolerated by the system but they shouldn't be
-  used as they can cause unexpected bugs.
- */
 final GoRouter router = GoRouter(
   initialLocation: '/login',
   routes: <RouteBase>[
@@ -23,11 +18,20 @@ final GoRouter router = GoRouter(
         return const LoginPage();
       },
     ),
-    GoRoute(
-      path: '/home',
-      builder: (BuildContext context, GoRouterState state) {
-        return const HomePage();
+    ShellRoute(
+      builder: (context, state, child) {
+        return DashboardLayout(child: child);
       },
+      routes: [
+        GoRoute(
+          path: '/dashboard/home',
+          builder: (context, state) => const DashboardHomePage(),
+        ),
+        GoRoute(
+          path: '/dashboard/add',
+          builder: (context, state) => const DashboardAddPage(),
+        ),
+      ],
     ),
   ],
 );
