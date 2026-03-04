18dc6036f103d8b68aea8b8141fcc8dcdf4b995d
Sayem-75 <ehtesam.alhaqq@gmail.com>
2026-03-04 17:04:44 -0600

    Manually updated schema.dart

--- frontend/search_cms/lib/core/database/schema.dart
+++ frontend/search_cms/lib/core/database/schema.dart
@@ -1,12 +1,54 @@
 import 'package:powersync/powersync.dart';
 
-const countersTable = 'counters';
+const sitesTable = 'site';
+const areasTable = 'area';
+const siteAreasTable = 'site_area';
+const unitsTable = 'unit';
+const levelsTable = 'level';
 
 const schema = Schema([
-  Table(countersTable, [
-    Column.integer('owner_id'), // String field for identifying record owner
-    Column.integer('count'), // Number field for counter value
-    Column.text('created_at'), // Timestamp field for record creation time
-    Column.text('modified_at'),
+  Table(sitesTable, [
+    Column.text('id'),
+    Column.text('name'),
+    Column.text('borden'),
+    Column.text('created_at'),
+    Column.text('updated_at'),
+  ]),
+
+  Table(areasTable, [
+    Column.text('id'),
+    Column.text('name'),
+    Column.text('created_at'),
+    Column.text('updated_at'),
+  ]),
+
+  Table(siteAreasTable, [
+    Column.text('site_id'),
+    Column.text('area_id'),
+  ]),
+
+  Table(unitsTable, [
+    Column.text('id'),
+    Column.text('site_id'),
+    Column.text('name'),
+    Column.text('created_at'),
+    Column.text('updated_at'),
+  ]),
+  
+  Table(levelsTable, [
+    Column.text('id'),
+    Column.text('unit_id'),
+    Column.text('parent_id'),
+    Column.text('name'),
+    Column.integer('up_limit'),
+    Column.integer('low_limit'),
+    Column.text('created_at'),
+    Column.text('updated_at'),
+    Column.text('level_char'),
+    Column.integer('level_int'),
   ]),
 ]);
+
+
+
+
