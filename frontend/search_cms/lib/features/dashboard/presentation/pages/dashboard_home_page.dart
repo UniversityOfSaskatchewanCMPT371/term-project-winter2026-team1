10f61ee5d09155e448e9c46baffdd9a0c43bea3e
Matt Berry <matthewjberry03@gmail.com>
2026-03-03 18:17:56 -0600

    Added import for Sizer and initial layout of titlecard

--- frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_home_page.dart
+++ frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:search_cms/core/utils/constants.dart';
+import 'package:sizer/sizer.dart';
 
 
 /// Main dashboard home page, showed by defualt after logging in
@@ -9,27 +10,35 @@ class DashboardHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RichText(
-        text: TextSpan(
-          style: TextStyle(
-            fontSize: 48.0,
-            height: 1.1,
-            color: AppColors.mainText,
-            letterSpacing: 1.0,
-          ),
-          children: const [
+    return Column(
+      children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          // Home title card
+          Text.rich(
             TextSpan(
               text: 'Home',
               style: TextStyle(
+                fontSize: 8.sp,
+                color: const Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.w300,
+              )
+            ),
+          ),
+          // Last updated text, should be dynamic in the future
+          Text.rich(
+            TextSpan(
+              text: 'Last Updated: 2024-06-01',
+              style: TextStyle(
+                fontSize: 6.sp,
                 color: AppColors.primaryBlue,
                 fontWeight: FontWeight.w300,
               ),
             )
-          ],
-          // TODO: Add more widgets here
+          )
+          ]
         ),
-      ),
+      ],
     );
   }
 }
