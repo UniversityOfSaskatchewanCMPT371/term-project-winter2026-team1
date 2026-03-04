9eed2c846cc902eaff44d6cbe3b88021954aab81
Matt Berry <matthewjberry03@gmail.com>
2026-03-03 18:54:20 -0600

    Created class for SearchToggle Inlcudes a super SearchToggle and a state manager. I think one or both of these will need to get moved to a bloc/cubit folder But I will circle back to that

--- frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_home_page.dart
+++ frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_home_page.dart
@@ -2,6 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:search_cms/core/utils/constants.dart';
 import 'package:sizer/sizer.dart';
 
+/// Class for the search / advanced search toggle buttons, which will be used in the dashboard home page
+/// I really expect that this should be in a different file lol
+class SearchToggle extends StatefulWidget {
+  const SearchToggle({super.key});
+
+  @override
+  State<SearchToggle> createState() => _SearchToggleState();
+}
+class _SearchToggleState extends State<SearchToggle> {
+  final List<bool> _selected = [true, false];
+
+  @override
+  Widget build(BuildContext context) {
+    return ToggleButtons(
+      direction: Axis.horizontal,
+      onPressed: (int index) {
+        setState(() {
+          // The button that is tapped is set to true, and the others to false.
+          for (int i = 0; i < _selected.length; i++) {
+            _selected[i] = i == index;
+          }
+        });
+      },
+      borderRadius: const BorderRadius.all(Radius.circular(12)),
+      
+      selectedBorderColor: Colors.black,
+      disabledBorderColor: Colors.black,
+      borderWidth: 1,
+      selectedColor: Colors.white,  // text colour when selected
+      fillColor: Colors.blueGrey,   // background colour when selected
+      disabledColor: Colors.grey[850],      // unselected text colour
+
+      constraints: const BoxConstraints(
+        minHeight: 40.0,
+        minWidth: 80.0,
+      ),
+      isSelected: _selected,
+      children: [Text("Search"), Text("Advanced Search")],
+    );
+  }
+}
+
 
 /// Main dashboard home page, showed by defualt after logging in
 /// Contains title card, search entry point, and data display widgets
@@ -37,6 +79,7 @@ class DashboardHomePage extends StatelessWidget {
               ),
             )
           ),
+
           const Divider(
             height: 5,
             thickness: 5,
@@ -44,6 +87,13 @@ class DashboardHomePage extends StatelessWidget {
             endIndent: 5,
             color: Color.fromARGB(255, 122, 122, 122),
           ),
+
+          // Search / Advanced Search Toggle
+          Row(crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SearchToggle()
+          ],
+          )
           ]
         ),
       ],
