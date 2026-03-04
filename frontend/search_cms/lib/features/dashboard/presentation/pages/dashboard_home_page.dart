27d33876b41a0e077ff082022bebcdf637281938
Matt Berry <matthewjberry03@gmail.com>
2026-03-04 15:23:26 -0600

    Adjusted home page to use Bloc pattern for state management

--- frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_home_page.dart
+++ frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_home_page.dart
@@ -1,4 +1,3 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:search_cms/core/utils/constants.dart';
@@ -7,6 +6,90 @@ import 'package:two_dimensional_scrollables/two_dimensional_scrollables.dart';
 
 import '../bloc/home_cubit.dart';
 import '../bloc/home_state.dart';
+
+
+/// Main dashboard home page, showed by defualt after logging in
+/// Contains title card, search entry point, and data display widgets
+class DashboardHomePage extends StatelessWidget {
+  const DashboardHomePage({super.key});
+  // int _selectedSearch = 0; // 0 = Search, 1 = Advanced Search
+  // Set<String> _selectedColumns = {}; // Set to hold selected columns for filtering
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<HomeCubit, HomeState>(
+      builder: (context, state) {
+        // Show loading indicator until cubit emits DashboardLoaded
+        if (state is! HomeLoaded) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        return Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              // Home title card
+              Text.rich(
+                TextSpan(
+                  text: 'Home',
+                  style: TextStyle(
+                    fontSize: 8.sp,
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                    fontWeight: FontWeight.w300,
+                  )
+                ),
+              ),
+              // Last updated text, should be dynamic in the future (or NUKED lol)
+              Text.rich(
+                TextSpan(
+                  text: 'Last Updated: 2024-06-01',
+                  style: TextStyle(
+                    fontSize: 6.sp,
+                    color: AppColors.primaryBlue,
+                    fontWeight: FontWeight.w300,
+                  ),
+                )
+              )
+              ]
+            ),
+
+            const Divider(
+              height: 5,
+              thickness: 5,
+              indent: 5,
+              endIndent: 5,
+              color: Color.fromARGB(255, 110, 110, 110),
+            ),
+
+            // Search / Advanced Search Toggle
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SearchToggle(onSelectionChanged: (index) {
+                  context.read<HomeCubit>().updateSearchToggle(index);
+                },),
+                FilterColumnsDropdown(onSelectionChanged: (columns) {
+                  context.read<HomeCubit>().updateSelectedColumns(columns);
+                },)
+              ]
+            ),
+
+            if (state.selectedSearch == 0)
+              const BasicSearchBar()
+            else
+              const BasicSearchBar() // TODO: replace with call to const AdvancedSearchPanel(),
+          
+
+            ,const SizedBox(height: 20), // Add some spacing between the search bar and the data table
+
+
+            DataTable()
+          ],
+        );
+      }
+    );
+  }
+}
+
 
 /// Class for the search / advanced search toggle buttons, which will be used in the dashboard home page
 /// I really expect that this should be in a different file lol
@@ -49,83 +132,6 @@ class _SearchToggleState extends State<SearchToggle> {
       isSelected: _selected,
       children: [Text("Search"), Text("Advanced Search")],
     );
-  }
-}
-
-
-/// Main dashboard home page, showed by defualt after logging in
-/// Contains title card, search entry point, and data display widgets
-class DashboardHomePage extends StatelessWidget {
-  const DashboardHomePage({super.key});
-  // int _selectedSearch = 0; // 0 = Search, 1 = Advanced Search
-  // Set<String> _selectedColumns = {}; // Set to hold selected columns for filtering
-
-  @override
-  Widget build(BuildContext context) {
-   return BlocProvider(
-    create: (_) => HomeCubit(),
-    child: Column(
-      children: [
-        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-          // Home title card
-          Text.rich(
-            TextSpan(
-              text: 'Home',
-              style: TextStyle(
-                fontSize: 8.sp,
-                color: const Color.fromARGB(255, 0, 0, 0),
-                fontWeight: FontWeight.w300,
-              )
-            ),
-          ),
-          // Last updated text, should be dynamic in the future (or NUKED lol)
-          Text.rich(
-            TextSpan(
-              text: 'Last Updated: 2024-06-01',
-              style: TextStyle(
-                fontSize: 6.sp,
-                color: AppColors.primaryBlue,
-                fontWeight: FontWeight.w300,
-              ),
-            )
-          )
-          ]
-        ),
-
-        const Divider(
-          height: 5,
-          thickness: 5,
-          indent: 5,
-          endIndent: 5,
-          color: Color.fromARGB(255, 110, 110, 110),
-        ),
-
-        // Search / Advanced Search Toggle
-        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SearchToggle(onSelectionChanged: (index) {
-              setState(() => _selectedSearch = index);
-            },),
-            FilterColumnsDropdown(onSelectionChanged: (columns) {
-              setState(() => _selectedColumns = columns);
-            },)
-          ]
-        ),
-
-        if (_selectedSearch == 0)
-          const BasicSearchBar()
-        else
-          const BasicSearchBar() // TODO: replace with call to const AdvancedSearchPanel(),
-      
-
-        ,const SizedBox(height: 20), // Add some spacing between the search bar and the data table
-
-
-        DataTable()
-      ],
-    )
-   );
   }
 }
 
