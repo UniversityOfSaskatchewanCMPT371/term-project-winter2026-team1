5ae37408b43f31b0022368fa3d5b8210b5a0e846
theo12705 <77030522+theo12705@users.noreply.github.com>
2026-03-03 21:06:23 -0600

    Fixed a bug where powersync does not work properly
    -> Added site schema for powersync sync rules
    -> Added sync rules to powersync for site
    -> Fixed a bug where powersync could not sync

--- backend/supabase/migrations/20260221231045_search_arch_table.sql
+++ backend/supabase/migrations/20260221231045_search_arch_table.sql
@@ -1,3 +1,5 @@
+CREATE PUBLICATION powersync FOR ALL TABLES;
+
 create table public.role (
   id uuid not null,
   created_at timestamp with time zone not null default (now() AT TIME ZONE 'utc'::text),
@@ -5,4 +7,13 @@ create table public.role (
   constraint role_pkey primary key (id),
   constraint role_id_fkey foreign KEY (id) references auth.users (id) on update CASCADE on delete CASCADE
 ) TABLESPACE pg_default;
+
+CREATE TABLE site (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT DEFAULT '', -- Can be empty
+    borden VARCHAR(8) NOT NULL UNIQUE, -- Limit 8 from the provided dump file
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now() -- For admin editing purposes
+);
+
 
