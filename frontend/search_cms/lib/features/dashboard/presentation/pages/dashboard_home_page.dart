3acdbfde1ea04cc44abb99427bbd713173a08331
Matt Berry <matthewjberry03@gmail.com>
2026-03-05 10:49:23 -0600

    Improved display of table

--- frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_home_page.dart
+++ frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_home_page.dart
@@ -303,30 +303,41 @@ class _FilterColumnsDropdownState extends State<FilterColumnsDropdown> {
 
     @override
     Widget build(BuildContext context) {
-      return Container(
-        color: Colors.grey[200],
-        child: Center(
-          child: TableView.builder(
-            columnCount: 4,
-            rowCount: 10,
-            columnBuilder: buildTableSpan,
-            rowBuilder: buildTableSpan,
+      return Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: TableView.builder(
+          columnCount: 4,
+          rowCount: 10,
+          columnBuilder: (index) => buildColumnSpan(index, context),
+          rowBuilder: buildRowSpan,
 
-            // TODO: placeholder - need to figure out how to render data from the backend here
-            cellBuilder:(BuildContext context, TableVicinity vicinity) {
-              return TableViewCell(
-                child: Center(
-                  child: Text('Cell ${vicinity.column} : ${vicinity.row}'),
+          // TODO: placeholder - need to figure out how to render data from the backend here
+          cellBuilder:(BuildContext context, TableVicinity vicinity) {
+            return TableViewCell(
+              child: Container(
+                decoration: BoxDecoration(
+                  // TODO: fix the overlap
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                  )
                 ),
-              );
-            },
-          ),
-        )
+                child: Text('Cell ${vicinity.column} : ${vicinity.row}'),
+              ),
+            );
+          },
+        ),
       );
     }
 
-    TableSpan buildTableSpan(int index){
-      // add row/col level decorations here
+    TableSpan buildRowSpan(int index){
+      // add row level decorations here
       return TableSpan(extent: FixedTableSpanExtent(50));
+    }
+
+    TableSpan buildColumnSpan(int index, BuildContext context){
+      // add col level decorations here
+      // TODO: replace 4 with dynamic column count based on search results, and adjust the extent accordingly
+      return TableSpan(extent: FractionalTableSpanExtent(1 / 4),);
     }
   }
