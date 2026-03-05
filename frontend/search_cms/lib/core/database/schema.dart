ea8b84ea4df2b320b01928ff144b24038e556f80
Sayem-75 <ehtesam.alhaqq@gmail.com>
2026-03-05 13:28:50 -0600

    Added comments in schema.dart to address PR review

--- frontend/search_cms/lib/core/database/schema.dart
+++ frontend/search_cms/lib/core/database/schema.dart
@@ -1,5 +1,6 @@
 import 'package:powersync/powersync.dart';
 
+// Read the tables
 const sitesTable = 'site';
 const areasTable = 'area';
 const siteAreasTable = 'site_area';
@@ -8,47 +9,46 @@ const levelsTable = 'level';
 
 const schema = Schema([
   Table(sitesTable, [
-    Column.text('id'),
-    Column.text('name'),
-    Column.text('borden'),
-    Column.text('created_at'),
-    Column.text('updated_at'),
+    Column.text('id'), // Uniquely generated ID (UUID)
+    Column.text('name'), // The name of a site. This can be empty
+    Column.text('borden'), // The Borden, which is like the name
+    Column.text('created_at'), // When a site was created
+    Column.text('updated_at'), // When was data last updated
   ]),
 
   Table(areasTable, [
-    Column.text('id'),
-    Column.text('name'),
-    Column.text('created_at'),
-    Column.text('updated_at'),
+    Column.text('id'), // Uniquely generated ID (UUID)
+    Column.text('name'), // The name of an area
+    Column.text('created_at'), // When an area was created
+    Column.text('updated_at'), // When was data last updated
   ]),
 
+  // An intermediate table. Takes site_id and area_id as PKs
   Table(siteAreasTable, [
     Column.text('site_id'),
     Column.text('area_id'),
   ]),
 
   Table(unitsTable, [
-    Column.text('id'),
-    Column.text('site_id'),
-    Column.text('name'),
-    Column.text('created_at'),
-    Column.text('updated_at'),
+    Column.text('id'), // Uniquely generated ID (UUID)
+    Column.text('site_id'), // A site can have multiple units
+    Column.text('name'), // The name of a unit
+    Column.text('created_at'), // When a unit was created
+    Column.text('updated_at'), // When was data last updated
   ]),
   
   Table(levelsTable, [
-    Column.text('id'),
-    Column.text('unit_id'),
-    Column.text('parent_id'),
-    Column.text('name'),
-    Column.integer('up_limit'),
-    Column.integer('low_limit'),
-    Column.text('created_at'),
-    Column.text('updated_at'),
-    Column.text('level_char'),
-    Column.integer('level_int'),
+    Column.text('id'), // Uniquely generated ID (UUID)
+    Column.text('unit_id'), // A unit can have multiple levels.
+    Column.text('parent_id'), //The top-most level in a unit can't have a parent
+    Column.text('name'), // The name of a level
+    Column.integer('up_limit'), // Depth in cm
+    Column.integer('low_limit'), // Depth in cm
+    Column.text('created_at'), // When a level was created
+    Column.text('updated_at'), // When was data last updated
+    Column.text('level_char'), // Some other arch data
+    Column.integer('level_int'), // Some other arch data
   ]),
 ]);
-
-
 
 
