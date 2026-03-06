bd46dec413adf3584d81d7340e57bedef82be6cd
Tyler Chow <tylerchow0@gmail.com>
2026-03-06 14:50:54 -0600

    skeleton for add data page
    Issue #127
    - created a skeleton for the custom widget for adding an entry to the data base
    - the skeleton is a work in progress
    - since I'm more comfortable with html and css, I played around with the element styling there to get numbers
    - hopefully I can convert it into flutter, but more likely I will have to do it from scratch in flutter and just use it as a reference

--- frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_add_page.dart
+++ frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_add_page.dart
@@ -1,9 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:search_cms/core/utils/constants.dart';
 
-class DashboardAddPage extends StatelessWidget {
-  const DashboardAddPage({super.key});
 
+
+
+/*
+Will be used to store the keys for for the text fields of widgets created by
+createAddDataWidget.
+If needed, can be used to store information about what type a text field
+will need to be converted to.
+*/
+class AddDataPageEntries{
+
+  static Map <String, AddDataPageEntries> dataEntries = {};
+
+  AddDataPageEntries(String title, List<String> textFieldKeys){
+    //TODO
+  }
+}
+
+/*
+ * Creates the widgets for adding data to the database
+ * preconditions: unique title and text field names such that
+ *    - find.byKey(ValueKey(title + "-" + textFieldNames[index])) from flutter test 
+ *      will refer to only one widget
+ *    - the title is unique to any other titles passed to this function
+ * postconditions: 
+ *    - the widget keys 'title + "-" + textFieldNames[index]' will be taken
+ *    - will add an entry to AddDataPageEntries mapping dataEntriesKeys[title] to 
+ *      an instance of AddDataPageEntries
+ * returns a widget for adding an entry to the data base for a table corrosponding to title
+ */
+Widget createAddDataWidget(String title, List<String> textFieldNames){
+  //TODO
+
+  return Container(); //may change the type of widget returned
+}
+
+
+class DashboardAddPage extends StatelessWidget { //TODO: potentially change type
+  const DashboardAddPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
