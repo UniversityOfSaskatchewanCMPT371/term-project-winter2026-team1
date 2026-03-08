69896879db373bba15d9eb317edc6b46627fd545
Tyler Chow <tylerchow0@gmail.com>
2026-03-08 15:17:58 -0600

    added logging for add data page frontend
    Issue #127
    - added a logger that reports when the user tries to add an entry to the data base

--- frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_add_page.dart
+++ frontend/search_cms/lib/features/dashboard/presentation/pages/dashboard_add_page.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart';
 import 'package:search_cms/core/utils/constants.dart';
+import 'package:logging/logging.dart';
 
 
-import 'package:flutter_test/flutter_test.dart';
+final Logger? _logger =
+      logLevel != Level.OFF ? Logger('Add data page UI') : null;
 
 void saveButtonClicked(String title){
   //AddDataPageEntries? dataEntry = AddDataPageEntries.dataEntries[title]; //un comment this out when business logic is started
   //implement when business logic is planned or when I can talk to them
 
+  _logger?.info("save button was clicked for $title widget");
+
+  
 }
 
 
