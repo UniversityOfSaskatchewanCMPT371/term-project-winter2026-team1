1d4141e61af232d66f3b1118808329b8edbf99cf
huayihuang <huh742@usask.ca>
2026-03-14 18:21:20 -0600

    Get all sites use case is complete.

---
+++ frontend/search_cms/lib/features/dashboard/domain/usecases/dashboard_usecases.dart
@@ -0,0 +1,8 @@
+import 'package:search_cms/features/dashboard/domain/usecases/get_all_sites_usecase.dart';
+
+// The collection of all use cases for the dashboard
+class DashboardUsecases {
+  final GetAllSitesUseCase getAllSitesUseCase;
+
+  DashboardUsecases({required this.getAllSitesUseCase});
+}
