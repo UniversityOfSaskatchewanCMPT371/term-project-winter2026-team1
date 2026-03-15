943b594a41adb28bbd480ba3ae6a8a8a2fa062f3
Jonathan Mutakura <jmutakura@gmail.com>
2026-03-14 21:12:31 -0600

    added the model & entities for the area table

---
+++ frontend/search_cms/lib/features/area/data/models/area_model.dart
@@ -0,0 +1,15 @@
+/*
+  The area model for the area data.
+
+  @param id The uuid for the area
+  @param name The name of the area
+ */
+class AreaModel {
+  final String id;
+  final String name;
+
+  AreaModel({
+    required this.id,
+    required this.name,
+  });
+}
---
+++ frontend/search_cms/lib/features/area/domain/entities/area_entity.dart
@@ -0,0 +1,15 @@
+/*
+  The area entity for the domain layer.
+
+  @param id The uuid for the area
+  @param name The name of the area
+ */
+class AreaEntity {
+  final String id;
+  final String name;
+
+  AreaEntity({
+    required this.id,
+    required this.name,
+  });
+}
