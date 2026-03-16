d8f72124927a8785aad0dc73f8c1301bed85ebf2
Yousif Warda (ytq183) <ytq183@mail.usask.ca>
2026-03-15 22:38:29 -0600

    Loaded,Loading state has been implemented

---
+++ frontend/search_cms/lib/features/dashboard/presentation/bloc/add_data_state.dart
@@ -0,0 +1,36 @@
+import 'package:equatable/equatable.dart';
+
+sealed class AddDataState extends Equatable {
+  const AddDataState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+// Initial state before the page takes to its loading state
+class AddDataInitial extends AddDataState {
+  const AddDataInitial();
+
+}
+
+// Adding a loading state for the add data page
+class AddDataLoading extends AddDataState {
+  const AddDataLoading();
+}
+
+//Loaded state for the the add data page
+//Makes sure that the Add Data page is fully loaded
+class AddDataLoaded extends AddDataState {
+
+  //Stores typed valued for each state for each specific column/field
+  //Field values can now be stored
+  final Map<String, String> fieldValues;
+
+  const AddDataLoaded({
+    this.fieldValues = const {},
+  });
+
+  @override
+  List<Object> get props => [fieldValues];
+}
+
