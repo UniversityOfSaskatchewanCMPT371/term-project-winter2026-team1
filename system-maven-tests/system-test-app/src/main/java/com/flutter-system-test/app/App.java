7f6393158a5f0d4bebb3b955468b41edff38d22d
theo12705 <77030522+theo12705@users.noreply.github.com>
2026-03-16 20:15:03 -0600

    Add Maven project for Appium system tests.
    -> Added Maven repository for junit tests
    -> Setup basic skeleton for maven project and it's associated
    pom file.
    -> Setup Junit dependencies for pom

---
+++ system-maven-tests/system-test-app/src/main/java/com/flutter-system-test/app/App.java
@@ -0,0 +1,10 @@
+package com.flutter-system-test.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
