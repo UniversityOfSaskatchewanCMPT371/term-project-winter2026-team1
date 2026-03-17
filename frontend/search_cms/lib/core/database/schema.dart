6e1c786853857e949b8e976e91e3d7f2d1b79c59
Sayem-75 <ehtesam.alhaqq@gmail.com>
2026-03-17 02:18:51 -0600

    Modified schema.dart

--- frontend/search_cms/lib/core/database/schema.dart
+++ frontend/search_cms/lib/core/database/schema.dart
@@ -1,40 +1,33 @@
 import 'package:powersync/powersync.dart';
 
-// Read the tables
-const sitesTable = 'site';
-const areasTable = 'area';
-const siteAreasTable = 'site_area';
-const unitsTable = 'unit';
-const levelsTable = 'level';
-
 const schema = Schema([
-  Table(sitesTable, [
+  Table('site', [
     Column.text('name'), // The name of a site. This can be empty
     Column.text('borden'), // The Borden, which is like the name
     Column.text('created_at'), // When a site was created
     Column.text('updated_at'), // When was data last updated
   ]),
 
-  Table(areasTable, [
+  Table('area', [
     Column.text('name'), // The name of an area
     Column.text('created_at'), // When an area was created
     Column.text('updated_at'), // When was data last updated
   ]),
 
   // An intermediate table. Takes site_id and area_id as PKs
-  Table(siteAreasTable, [
+  Table('site_area', [
     Column.text('site_id'),
     Column.text('area_id'),
   ]),
 
-  Table(unitsTable, [
+  Table('unit', [
     Column.text('site_id'), // A site can have multiple units
     Column.text('name'), // The name of a unit
     Column.text('created_at'), // When a unit was created
     Column.text('updated_at'), // When was data last updated
   ]),
   
-  Table(levelsTable, [
+  Table('level', [
     Column.text('unit_id'), // A unit can have multiple levels.
     Column.text('parent_id'), //The top-most level in a unit can't have a parent
     Column.text('name'), // The name of a level
