38f0f16f39ec23a430b6512b963720fe1ea0e433
huayihuang <huh742@usask.ca>
2026-03-18 01:27:53 -0600

    Get areas repository is now complete

--- frontend/search_cms/lib/features/dashboard/domain/repositories/abstract_get_all_areas_repository.dart
+++ frontend/search_cms/lib/features/dashboard/domain/repositories/abstract_get_all_areas_repository.dart
@@ -1,18 +1,18 @@
 import 'package:search_cms/core/utils/class_templates/result.dart';
 
 /*
-  The repository interface for retrieving all sites
+  The repository contract for retrieving all areas
  */
-abstract class AbstractGetAllSitesRepository {
+abstract class AbstractGetAllAreasRepository {
   /*
-    Retrieves all Sites in the system
+    Retrieves all Areas in the system
 
-    @return A Success if the fetch is successful, containing the list of site
+    @return A Success if the fetch is successful, containing the list of area
       entities or Failure containing the errorMessage otherwise
 
     Preconditions:
       (1) PowerSync database is initialized
       (2) The user must be authenticated
   */
-  Future<Result> getAllSites();
+  Future<Result> getAllAreas();
 }
