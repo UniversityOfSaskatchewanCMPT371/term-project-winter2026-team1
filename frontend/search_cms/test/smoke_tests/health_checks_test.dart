95778250bea723d749eff385179458e0f243f3b0
Talha Hassan <qzl241@usask.ca>
2026-03-19 14:39:40 -0600

    Move smoke tests into a seperate folder

--- frontend/search_cms/test/smoke_tests/health_checks_test.dart
+++ frontend/search_cms/test/smoke_tests/health_checks_test.dart
@@ -3,6 +3,14 @@ import 'package:logging/logging.dart';
 
 void healthChecksTest(Logger logger) {
   group("Health Checks", () {
+    /*
+      Preconditions:
+      - Supabase instance must be running locally
+      - Supabase health endpoint must be reachable: 127.0.0.1:54321
+
+      Postconditions:
+      - Confirms backend authentication system is up and running
+    */
     test("Attempt http ping to Supabase", (
     ) async {
       logger.info("Attempting to ping Supabase");
