3a1afe9bb65df95f5d7ead42bcfd582bdcc2d481
Talha Hassan <qzl241@usask.ca>
2026-03-19 16:55:24 -0600

    Moved smoked tests back into integration_test folder

--- frontend/search_cms/integration_test/smoke_test.dart
+++ frontend/search_cms/integration_test/smoke_test.dart
@@ -2,10 +2,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:logging/logging.dart';
 
-import '../test/smoke_tests/basic_functionality_test.dart';
-import '../test/smoke_tests/health_checks_test.dart';
-import '../test/smoke_tests/render_pages_test.dart';
-import '../test/smoke_tests/sync_test.dart';
+import 'smoke_tests/basic_functionality_test.dart';
+import 'smoke_tests/health_checks_test.dart';
+import 'smoke_tests/render_pages_test.dart';
+import 'smoke_tests/sync_test.dart';
 
 void main() async {
   final Logger logger = Logger('Smoke Test');
