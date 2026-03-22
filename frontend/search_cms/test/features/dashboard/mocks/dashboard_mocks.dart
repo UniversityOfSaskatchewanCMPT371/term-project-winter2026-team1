afef72beee909df2a60b41e89d43ef8b79998b23
goyalhimanshu345 <goyalhimanshu345@gmail.com>
2026-03-22 00:24:13 -0600

    Mocks for Dashboard feature

--- frontend/search_cms/test/features/dashboard/mocks/dashboard_mocks.dart
+++ frontend/search_cms/test/features/dashboard/mocks/dashboard_mocks.dart
@@ -6,19 +6,58 @@ and makes build_runner output predictable and easy to regenerate.
 To regenerate mocks : dart run build_runner build --delete-conflicting-outputs
 */
 
+library dashboard_mocks;
+
 import 'package:mockito/annotations.dart';
 import 'package:powersync/powersync.dart';
-import 'package:search_cms/features/dashboard/data/data_sources/abstract_dashboard_api.dart';
-import 'package:search_cms/features/dashboard/data/data_sources/abstract_get_all_sites_api.dart';
-import 'package:search_cms/features/dashboard/domain/repositories/abstract_dashboard_repository.dart';
-import 'package:search_cms/features/dashboard/domain/repositories/abstract_get_all_sites_repository.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+// APIs
+import 'package:search_cms/features/dashboard/data/data_sources/abstract_dashboard_api.dart';
+import 'package:search_cms/features/dashboard/data/data_sources/abstract_get_all_areas_api.dart';
+import 'package:search_cms/features/dashboard/data/data_sources/abstract_get_all_levels_api.dart';
+import 'package:search_cms/features/dashboard/data/data_sources/abstract_get_all_sites_api.dart';
+import 'package:search_cms/features/dashboard/data/data_sources/abstract_get_all_units_api.dart';
+
+// Repositories
+import 'package:search_cms/features/dashboard/domain/repositories/abstract_dashboard_repository.dart';
+import 'package:search_cms/features/dashboard/domain/repositories/abstract_get_all_areas_repository.dart';
+import 'package:search_cms/features/dashboard/domain/repositories/abstract_get_all_levels_repository.dart';
+import 'package:search_cms/features/dashboard/domain/repositories/abstract_get_all_sites_repository.dart';
+import 'package:search_cms/features/dashboard/domain/repositories/abstract_get_all_units_repository.dart';
+
+// Usecases
+import 'package:search_cms/features/dashboard/domain/usecases/dashboard_usecases.dart';
+import 'package:search_cms/features/dashboard/domain/usecases/get_all_areas_usecase.dart';
+import 'package:search_cms/features/dashboard/domain/usecases/get_all_levels_usecase.dart';
+import 'package:search_cms/features/dashboard/domain/usecases/get_all_site_areas_usecase.dart';
+import 'package:search_cms/features/dashboard/domain/usecases/get_all_sites_usecase.dart';
+import 'package:search_cms/features/dashboard/domain/usecases/get_all_units_usecase.dart';
+
 @GenerateMocks([
+  // APIs
   AbstractDashboardApi,
   AbstractGetAllSitesApi,
+  AbstractGetAllAreasApi,
+  AbstractGetAllLevelsApi,
+  AbstractGetAllUnitsApi,
+
+  // Repositories
   AbstractDashboardRepository,
   AbstractGetAllSitesRepository,
+  AbstractGetAllAreasRepository,
+  AbstractGetAllLevelsRepository,
+  AbstractGetAllUnitsRepository,
+
+  // Usecases
+  GetAllAreasUseCase,
+  GetAllLevelsUseCase,
+  GetAllSiteAreasUseCase,
+  GetAllSitesUseCase,
+  GetAllUnitsUseCase,
+  DashboardUsecases,
+
+  // Others
   PowerSyncDatabase,
   SupabaseClient,
 ])
