ada939cf188f23327f26d277cf5bd93cbb616196
Talha Hassan <qzl241@usask.ca>
2026-03-22 14:30:53 -0600

    move initInjections back into smoke test and remove other instances

--- frontend/search_cms/integration_test/smoke_tests/health_checks_test.dart
+++ frontend/search_cms/integration_test/smoke_tests/health_checks_test.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 import 'package:logging/logging.dart';
 import 'package:powersync/powersync.dart';
 import 'package:search_cms/core/app_config.dart';
-import 'package:search_cms/core/injections.dart';
 import 'package:search_cms/core/utils/constants.dart';
 
 void healthChecksTest(Logger logger) {
@@ -50,8 +49,6 @@ void healthChecksTest(Logger logger) {
     test("Attempt ping to Powersync", (
     ) async {
       logger.info("Attempting to ping Powersync");
-
-      await initInjections();
 
       int attempts = 0;
       const int maxAttempts = 100;
