c73dc9686215407d0d421f36330a2d98514872ae
Tyler Chow <tylerchow0@gmail.com>
2026-03-22 21:10:21 -0600

    updated code using the proper version of copilot
    - commented out all lines from GitHub Copilot Chat VSCode extension
    - redid the feature using the proper version of copilot (basically did the same thing but made it a function to be called in tester.pumpWidget() instead of having it in each test

--- frontend/search_cms/test/features/dashboard/presentation/pages/add_data_page_widget_test.dart
+++ frontend/search_cms/test/features/dashboard/presentation/pages/add_data_page_widget_test.dart
@@ -4,20 +4,30 @@ import 'package:search_cms/features/dashboard/presentation/pages/dashboard_add_p
 import 'package:sizer/sizer.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+  // TestWidgetsFlutterBinding.ensureInitialized();
+
+  // for creating the widget to be tested
+  Widget createTestWidget(Widget child) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(home: child);
+      },
+    );
+  }
 
   // widget tests for the add data page
   group("widget tests for the add data page", () {
     // widget test for the save button
     testWidgets("checking that there is a save button", (tester) async {
       // await tester.pumpWidget(const DashboardAddPage());
-      await tester.pumpWidget(
-        Sizer(
-          builder: (context, orientation, deviceType) => const MaterialApp(
-            home: DashboardAddPage(), // the page being tested
-          ),
-        ),
-      );
+      // await tester.pumpWidget(
+      //   Sizer(
+      //     builder: (context, orientation, deviceType) => const MaterialApp(
+      //       home: DashboardAddPage(), // the page being tested
+      //     ),
+      //   ),
+      // );
+      await tester.pumpWidget(createTestWidget(const DashboardAddPage()));
 
       // ensuring there is at least 1 save button
       expect(find.byKey(Key("saveButton")), findsAny);
@@ -26,13 +36,14 @@ void main() {
     // widget test for the reset button
     testWidgets("checking that there is a reset button", (tester) async {
       // await tester.pumpWidget(const DashboardAddPage());
-      await tester.pumpWidget(
-        Sizer(
-          builder: (context, orientation, deviceType) => const MaterialApp(
-            home: DashboardAddPage(), // the page being tested
-          ),
-        ),
-      );
+      // await tester.pumpWidget(
+      //   Sizer(
+      //     builder: (context, orientation, deviceType) => const MaterialApp(
+      //       home: DashboardAddPage(), // the page being tested
+      //     ),
+      //   ),
+      // );
+      await tester.pumpWidget(createTestWidget(const DashboardAddPage()));
 
       // ensuring there is at least 1 reset button
       expect(find.byKey(Key("resetButton")), findsAny);
