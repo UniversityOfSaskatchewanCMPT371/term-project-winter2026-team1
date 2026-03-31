e5d7326bfd52458a00d13737f9c16608af291c48
Matt Berry <matthewjberry03@gmail.com>
2026-03-30 18:27:20 -0600

    modified all level insert API files unitId is now unitName parentId is now parentName query now resolves unitName -> unitId, parentName -> parentId (in non null)

--- frontend/search_cms/lib/features/dashboard/domain/usecases/insert_level_usecase.dart
+++ frontend/search_cms/lib/features/dashboard/domain/usecases/insert_level_usecase.dart
@@ -18,13 +18,11 @@ class InsertLevelUsecase {
   /*
     Inserts a new Level in the system
 
-    @param unitId A valid UUID reference to an existing unit
+    @param unitName A valid UUID reference to an existing unit
     @param name A non-empty name string for the level
     @param upLimit An integer representing the upper depth limit in cm
     @param lowLimit An integer representing the lower depth limit in cm
-    @param parentId An optional reference to an existing level which is the level's parent
-    @param levelChar An optional string representing some other archeological data
-    @param levelInt An optional integer representing some other archeological data
+    @param parentName An optional reference to an existing level which is the level's parent
 
     @return A Success if the insert is successful, or a Failure
       containing the errorMessage
@@ -32,16 +30,14 @@ class InsertLevelUsecase {
     Preconditions:
       (1) PowerSync database is initialized
       (2) The user must be authenticated
-      (3) unitId.isNotEmpty && name.isNotEmpty && upLimit <= lowLimit
+      (3) unitName.isNotEmpty && name.isNotEmpty && upLimit <= lowLimit
   */
   Future<Result> call({
-    required String unitId,
+    required String unitName,
     required String name,
     required int upLimit,
     required int lowLimit,
-    String? parentId,
-    String? levelChar,
-    int? levelInt,
+    String? parentName,
   }) async {
     _logger.finer('Insert level use case: Inserting level into PowerSync '
         'Database start');
@@ -50,13 +46,11 @@ class InsertLevelUsecase {
     assert(getIt<SupabaseClient>().auth.currentSession != null);
 
     Result result = await _repository.insertLevel(
-      unitId: unitId,
+      unitName: unitName,
       name: name,
       upLimit: upLimit,
       lowLimit: lowLimit,
-      parentId: parentId,
-      levelChar: levelChar,
-      levelInt: levelInt,
+      parentName: parentName,
     );
 
     _logger.finer('Insert level use case: Inserting level into PowerSync '
